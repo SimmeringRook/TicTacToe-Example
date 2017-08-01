a9aa8e67b60e062b768f2a134daff64f15c0494a
Thomas Knudson <knudsont6467@student.faytechcc.edu>
2017-08-01 16:14:32 -0700

    v1.0
    This is the base example.

---
+++ TicTacToe/TicTacToe/TicTacToeForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.currentPlayerLabel = new System.Windows.Forms.Label();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.gameTile1 = new System.Windows.Forms.Button();
+            this.gameTile2 = new System.Windows.Forms.Button();
+            this.gameTile3 = new System.Windows.Forms.Button();
+            this.gameTile6 = new System.Windows.Forms.Button();
+            this.gameTile5 = new System.Windows.Forms.Button();
+            this.gameTile4 = new System.Windows.Forms.Button();
+            this.gameTile9 = new System.Windows.Forms.Button();
+            this.gameTile8 = new System.Windows.Forms.Button();
+            this.gameTile7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // currentPlayerLabel
+            // 
+            this.currentPlayerLabel.AutoSize = true;
+            this.currentPlayerLabel.Location = new System.Drawing.Point(312, 37);
+            this.currentPlayerLabel.Name = "currentPlayerLabel";
+            this.currentPlayerLabel.Size = new System.Drawing.Size(91, 25);
+            this.currentPlayerLabel.TabIndex = 0;
+            this.currentPlayerLabel.Text = "X\'s Turn";
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(57, 28);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(140, 43);
+            this.newGameButton.TabIndex = 1;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // gameTile1
+            // 
+            this.gameTile1.Location = new System.Drawing.Point(257, 238);
+            this.gameTile1.Name = "gameTile1";
+            this.gameTile1.Size = new System.Drawing.Size(64, 64);
+            this.gameTile1.TabIndex = 2;
+            this.gameTile1.Text = " ";
+            this.gameTile1.UseVisualStyleBackColor = true;
+            this.gameTile1.Click += new System.EventHandler(this.gameTile1_Click);
+            // 
+            // gameTile2
+            // 
+            this.gameTile2.Location = new System.Drawing.Point(327, 238);
+            this.gameTile2.Name = "gameTile2";
+            this.gameTile2.Size = new System.Drawing.Size(64, 64);
+            this.gameTile2.TabIndex = 3;
+            this.gameTile2.Text = " ";
+            this.gameTile2.UseVisualStyleBackColor = true;
+            this.gameTile2.Click += new System.EventHandler(this.gameTile2_Click);
+            // 
+            // gameTile3
+            // 
+            this.gameTile3.Location = new System.Drawing.Point(397, 238);
+            this.gameTile3.Name = "gameTile3";
+            this.gameTile3.Size = new System.Drawing.Size(64, 64);
+            this.gameTile3.TabIndex = 4;
+            this.gameTile3.Text = " ";
+            this.gameTile3.UseVisualStyleBackColor = true;
+            this.gameTile3.Click += new System.EventHandler(this.gameTile3_Click);
+            // 
+            // gameTile6
+            // 
+            this.gameTile6.Location = new System.Drawing.Point(397, 308);
+            this.gameTile6.Name = "gameTile6";
+            this.gameTile6.Size = new System.Drawing.Size(64, 64);
+            this.gameTile6.TabIndex = 7;
+            this.gameTile6.Text = " ";
+            this.gameTile6.UseVisualStyleBackColor = true;
+            this.gameTile6.Click += new System.EventHandler(this.gameTile6_Click);
+            // 
+            // gameTile5
+            // 
+            this.gameTile5.Location = new System.Drawing.Point(327, 308);
+            this.gameTile5.Name = "gameTile5";
+            this.gameTile5.Size = new System.Drawing.Size(64, 64);
+            this.gameTile5.TabIndex = 6;
+            this.gameTile5.Text = " ";
+            this.gameTile5.UseVisualStyleBackColor = true;
+            this.gameTile5.Click += new System.EventHandler(this.gameTile5_Click);
+            // 
+            // gameTile4
+            // 
+            this.gameTile4.Location = new System.Drawing.Point(257, 308);
+            this.gameTile4.Name = "gameTile4";
+            this.gameTile4.Size = new System.Drawing.Size(64, 64);
+            this.gameTile4.TabIndex = 5;
+            this.gameTile4.Text = " ";
+            this.gameTile4.UseVisualStyleBackColor = true;
+            this.gameTile4.Click += new System.EventHandler(this.gameTile4_Click);
+            // 
+            // gameTile9
+            // 
+            this.gameTile9.Location = new System.Drawing.Point(397, 378);
+            this.gameTile9.Name = "gameTile9";
+            this.gameTile9.Size = new System.Drawing.Size(64, 64);
+            this.gameTile9.TabIndex = 10;
+            this.gameTile9.Text = " ";
+            this.gameTile9.UseVisualStyleBackColor = true;
+            this.gameTile9.Click += new System.EventHandler(this.gameTile9_Click);
+            // 
+            // gameTile8
+            // 
+            this.gameTile8.Location = new System.Drawing.Point(327, 378);
+            this.gameTile8.Name = "gameTile8";
+            this.gameTile8.Size = new System.Drawing.Size(64, 64);
+            this.gameTile8.TabIndex = 9;
+            this.gameTile8.Text = " ";
+            this.gameTile8.UseVisualStyleBackColor = true;
+            this.gameTile8.Click += new System.EventHandler(this.gameTile8_Click);
+            // 
+            // gameTile7
+            // 
+            this.gameTile7.Location = new System.Drawing.Point(257, 378);
+            this.gameTile7.Name = "gameTile7";
+            this.gameTile7.Size = new System.Drawing.Size(64, 64);
+            this.gameTile7.TabIndex = 8;
+            this.gameTile7.Text = " ";
+            this.gameTile7.UseVisualStyleBackColor = true;
+            this.gameTile7.Click += new System.EventHandler(this.gameTile7_Click);
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 681);
+            this.Controls.Add(this.gameTile9);
+            this.Controls.Add(this.gameTile8);
+            this.Controls.Add(this.gameTile7);
+            this.Controls.Add(this.gameTile6);
+            this.Controls.Add(this.gameTile5);
+            this.Controls.Add(this.gameTile4);
+            this.Controls.Add(this.gameTile3);
+            this.Controls.Add(this.gameTile2);
+            this.Controls.Add(this.gameTile1);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.currentPlayerLabel);
+            this.Name = "TicTacToeForm";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label currentPlayerLabel;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button gameTile1;
+        private System.Windows.Forms.Button gameTile2;
+        private System.Windows.Forms.Button gameTile3;
+        private System.Windows.Forms.Button gameTile6;
+        private System.Windows.Forms.Button gameTile5;
+        private System.Windows.Forms.Button gameTile4;
+        private System.Windows.Forms.Button gameTile9;
+        private System.Windows.Forms.Button gameTile8;
+        private System.Windows.Forms.Button gameTile7;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/TicTacToeForm.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Windows.Forms;
+
+/* Tic Tac Toe v1.0
+ * Thomas Knudson - 01 Aug 2017
+ * 
+ * This is a simple example of re-creating Tic Tac Toe using C# and WinForms.
+ * 
+ * Upon running the program, the game board (3x3 box of buttons, "gameTile") is disabled.
+ * The player who desires to be Player 1 (X), must click the New Game button. newGameButton
+ * has a click event which handles all the logic for starting a new game (either from a fresh
+ * start, or after a match has been completed).
+ * 
+ * When starting a new game:
+ *  -currentPlayerMark is set to "X" (for player 1)
+ *  -Each gameTile's Text property is set to " " (Empty)
+ *  -The game board is enabled (meaning each gameTile can respond to the players clicking on them)
+ * 
+ * Player's Turn:
+ *  -The player choose which tile to mark, and then clicks on it
+ *  -We check to make sure the title has not been marked before
+ *      If it is empty:
+ *          -The Current Player claims that tile
+ *      Otherwise:
+ *          -It remains the Current Player's turn, until they make a valid move
+ *  -This ends the current player's turn.
+ *  
+ * Between Turns:
+ *  -We check to see if the game has ended (3 in a row, column, or diagonally)
+ *      If a victory condition has been met:
+ *          -Disable the game board
+ *          -Display a message to the Players that the Current Player has won
+ *      If the game is a draw:
+ *          -Disable the game board
+ *          -Display a message to the Players
+ *      Otherwise:
+ *          -Change currentPlayerMark to the next player's mark
+ */
+
+namespace TicTacToe
+{
+    public partial class TicTacToeForm : Form
+    {
+        //Class variable that holds the mark for Player 1 (X) or Player 2 (O)
+        private string currentPlayerMark = "";
+
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+
+            //Disable the game board until the New Game button has been clicked
+            ToggleGameBoard(false);
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            //Set the Mark for player 1 - "X"
+            currentPlayerMark = "X";
+
+            //Make sure all the tiles are clear of player marks
+            ResetGameBoard();
+
+            //Allow the game tiles on the board to be clicked
+            ToggleGameBoard(true);
+        }
+
+        private void ChangePlayers()
+        {
+            //Change Marks between player 1 and player 2
+            if (currentPlayerMark.Equals("X"))
+            {
+                currentPlayerMark = "O";
+            }
+            else
+            {
+                currentPlayerMark = "X";
+            }
+
+            //Update the label, informing the player of whose turn it is
+            currentPlayerLabel.Text = currentPlayerMark + "'s Turn";
+        }
+        
+        private void NextTurn()
+        {
+            //Check to see if the current player has won
+            if (CheckForHorizontalVictory() || CheckForVerticalVictory() || CheckForDiagonalVictory())
+            {
+                //The current player has won
+
+                //Disable the game board
+                ToggleGameBoard(false);
+
+                //Inform the players that the current player has won
+                MessageBox.Show(currentPlayerMark + " wins!");
+            }
+            else if (CheckForDraw())
+            {
+                //There are no moves remaining
+
+                //Disable the game board
+                ToggleGameBoard(false);
+
+                //Inform the players the match is over
+                MessageBox.Show("The Game is a Draw!");
+            }
+            else
+            {
+                //No one has won yet, change players
+                ChangePlayers();
+            }
+        }
+
+        private void ToggleGameBoard(bool enable)
+        {
+            //This method changes the Enabled state of each game tile on the board
+            //if the arguement, "enable" is set to true:
+            //Players will be able to click on the board
+            //otherwise, the players will be unable to click any of the tiles
+            gameTile1.Enabled = enable;
+            gameTile2.Enabled = enable;
+            gameTile3.Enabled = enable;
+            gameTile4.Enabled = enable;
+            gameTile5.Enabled = enable;
+            gameTile6.Enabled = enable;
+            gameTile7.Enabled = enable;
+            gameTile8.Enabled = enable;
+            gameTile9.Enabled = enable;
+        }
+
+        private void ResetGameBoard()
+        {
+            //This method erases all previous player moves
+            //to allow for a new game to begin
+            gameTile1.Text = " ";
+            gameTile2.Text = " ";
+            gameTile3.Text = " ";
+            gameTile4.Text = " ";
+            gameTile5.Text = " ";
+            gameTile6.Text = " ";
+            gameTile7.Text = " ";
+            gameTile8.Text = " ";
+            gameTile9.Text = " ";
+        }
+
+        #region Victory Conditions
+        private bool CheckForHorizontalVictory()
+        {
+            bool horizontalVictory = false;
+
+            //This method checks each row, to see if the current player controls all 3 tiles
+            //If the player does control them, return true - the current player has won!
+            if (gameTile1.Text.Equals(currentPlayerMark) && gameTile2.Text.Equals(currentPlayerMark) && gameTile3.Text.Equals(currentPlayerMark))
+            {
+                horizontalVictory = true;
+            }
+            else if (gameTile4.Text.Equals(currentPlayerMark) && gameTile5.Text.Equals(currentPlayerMark) && gameTile6.Text.Equals(currentPlayerMark))
+            {
+                horizontalVictory = true;
+            }
+            else if (gameTile7.Text.Equals(currentPlayerMark) && gameTile8.Text.Equals(currentPlayerMark) && gameTile9.Text.Equals(currentPlayerMark))
+            {
+                horizontalVictory = true;
+            }
+            
+            //If the current player does not control all 3 tiles in any of the rows, return false
+            return horizontalVictory;
+        }
+
+        private bool CheckForVerticalVictory()
+        {
+            bool verticalVictory = false;
+
+            //This method checks each column, to see if the current player controls all 3 tiles
+            //If the player does control them, return true - the current player has won!
+
+            if (gameTile1.Text.Equals(currentPlayerMark) && gameTile4.Text.Equals(currentPlayerMark) && gameTile7.Text.Equals(currentPlayerMark))
+            {
+                verticalVictory = true;
+            }
+            else if (gameTile2.Text.Equals(currentPlayerMark) && gameTile5.Text.Equals(currentPlayerMark) && gameTile8.Text.Equals(currentPlayerMark))
+            {
+                verticalVictory = true;
+            }
+            else if (gameTile3.Text.Equals(currentPlayerMark) && gameTile6.Text.Equals(currentPlayerMark) && gameTile9.Text.Equals(currentPlayerMark))
+            {
+                verticalVictory = true;
+            }
+
+            //If the current player does not control all 3 tiles in any of the columns, return false
+            return verticalVictory;
+        }
+
+        private bool CheckForDiagonalVictory()
+        {
+            bool diagonalVictory = false;
+
+            //This method checks both diagonal possibilities;
+            //If the current player controls one, return true - the current player has won!
+
+            /* 1   3
+             *   5  
+             * 7   9
+             */ 
+
+            if (gameTile1.Text.Equals(currentPlayerMark) && gameTile5.Text.Equals(currentPlayerMark) && gameTile9.Text.Equals(currentPlayerMark))
+            {
+                diagonalVictory = true;
+            }
+            else if (gameTile3.Text.Equals(currentPlayerMark) && gameTile5.Text.Equals(currentPlayerMark) && gameTile7.Text.Equals(currentPlayerMark))
+            {
+                diagonalVictory = true;
+            }
+
+            return diagonalVictory;
+        }
+
+        #endregion
+
+        private bool CheckForDraw()
+        {
+            //TODO: Improve checking such that, once none of the victory conditions can be met, the game is declared
+            //a draw
+
+            if (gameTile1.Text.Equals(" ") == false && gameTile1.Text.Equals(" ") == false && gameTile1.Text.Equals(" ") == false &&
+                gameTile1.Text.Equals(" ") == false && gameTile1.Text.Equals(" ") == false && gameTile1.Text.Equals(" ") == false &&
+                gameTile1.Text.Equals(" ") == false && gameTile1.Text.Equals(" ") == false && gameTile1.Text.Equals(" ") == false)
+            {
+                //All tiles have been claimed, but no one has won
+                //It is a draw
+                return true;
+            }
+
+            //Not all of the tiles have been claimed yet
+            return false;
+        }
+
+        #region Game Tiles
+        private void gameTile1_Click(object sender, EventArgs e)
+        {
+            //Make sure the tile is empty
+            if (gameTile1.Text.Equals(" "))
+            {
+                //If it is, award the tile to the current player
+                gameTile1.Text = currentPlayerMark;
+
+                //End the current player's turn
+                NextTurn();
+            }
+        }
+
+        private void gameTile2_Click(object sender, EventArgs e)
+        {
+            //Make sure the tile is empty
+            if (gameTile2.Text.Equals(" "))
+            {
+                //If it is, award the tile to the current player
+                gameTile2.Text = currentPlayerMark;
+
+                //End the current player's turn
+                NextTurn();
+            }
+        }
+
+        private void gameTile3_Click(object sender, EventArgs e)
+        {
+            //Make sure the tile is empty
+            if (gameTile3.Text.Equals(" "))
+            {
+                //If it is, award the tile to the current player
+                gameTile3.Text = currentPlayerMark;
+
+                //End the current player's turn
+                NextTurn();
+            }
+        }
+
+        private void gameTile4_Click(object sender, EventArgs e)
+        {
+            //Make sure the tile is empty
+            if (gameTile4.Text.Equals(" "))
+            {
+                //If it is, award the tile to the current player
+                gameTile4.Text = currentPlayerMark;
+
+                //End the current player's turn
+                NextTurn();
+            }
+        }
+
+        private void gameTile5_Click(object sender, EventArgs e)
+        {
+            //Make sure the tile is empty
+            if (gameTile5.Text.Equals(" "))
+            {
+                //If it is, award the tile to the current player
+                gameTile5.Text = currentPlayerMark;
+
+                //End the current player's turn
+                NextTurn();
+            }
+        }
+
+        private void gameTile6_Click(object sender, EventArgs e)
+        {
+            //Make sure the tile is empty
+            if (gameTile6.Text.Equals(" "))
+            {
+                //If it is, award the tile to the current player
+                gameTile6.Text = currentPlayerMark;
+
+                //End the current player's turn
+                NextTurn();
+            }
+        }
+
+        private void gameTile7_Click(object sender, EventArgs e)
+        {
+            //Make sure the tile is empty
+            if (gameTile7.Text.Equals(" "))
+            {
+                //If it is, award the tile to the current player
+                gameTile7.Text = currentPlayerMark;
+
+                //End the current player's turn
+                NextTurn();
+            }
+        }
+
+        private void gameTile8_Click(object sender, EventArgs e)
+        {
+            //Make sure the tile is empty
+            if (gameTile8.Text.Equals(" "))
+            {
+                //If it is, award the tile to the current player
+                gameTile8.Text = currentPlayerMark;
+
+                //End the current player's turn
+                NextTurn();
+            }
+        }
+
+        private void gameTile9_Click(object sender, EventArgs e)
+        {
+            //Make sure the tile is empty
+            if (gameTile9.Text.Equals(" "))
+            {
+                //If it is, award the tile to the current player
+                gameTile9.Text = currentPlayerMark;
+
+                //End the current player's turn
+                NextTurn();
+            }
+        }
+        #endregion
+
+    }
+}
